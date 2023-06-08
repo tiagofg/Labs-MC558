be702e8741d6a74cf1d30e9454e4f83f83af8c26
tiagofg <tiagofg@Ubuntu.myguest.virtualbox.org>
2023-06-08 16:01:42 -0300

    Commit inicial t6

---
+++ t6/t6.c
@@ -0,0 +1,19 @@
+#include "shortest_path.h"
+
+int main(void) {
+    Grafo *g = novoGrafo(4);
+    adicionaArco(g, 0, 1, 2);
+    adicionaArco(g, 0, 2, 1);
+    adicionaArco(g, 1, 3, 3);
+    adicionaArco(g, 2, 3, 6);
+
+    printf("Grafo gerado:\n");
+    printGrafo(g);
+
+    int *dist = caminhoMinimo(g, 0, 3);
+    printf("Distância mínima entre 0 e 3: %d\n", dist[3]);
+    free(dist);
+    destroiGrafo(g);
+
+    return 0;
+}
