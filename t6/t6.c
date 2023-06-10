42281e2d663cdb776b7422ed96c4a90a4dab3a84
tiagofg <tiagofg@Ubuntu.myguest.virtualbox.org>
2023-06-10 00:34:00 -0300

    Solução parcial

--- t6/t6.c
+++ t6/t6.c
@@ -1,19 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "shortest_path.h"
 
-int main(void) {
-    Grafo *g = novoGrafo(4);
-    adicionaArco(g, 0, 1, 2);
-    adicionaArco(g, 0, 2, 1);
-    adicionaArco(g, 1, 3, 3);
-    adicionaArco(g, 2, 3, 6);
+typedef struct {
+    int valor;
+    int peso;
+    int qtde;
+} Moeda;
 
-    printf("Grafo gerado:\n");
-    printGrafo(g);
+Grafo* criarGrafo(Moeda* moedas, int numMoedas, int numVertices, int troco) {
+    Grafo* g = novoGrafo(numVertices);
 
-    int *dist = caminhoMinimo(g, 0, 3);
-    printf("Distância mínima entre 0 e 3: %d\n", dist[3]);
-    free(dist);
+    int* verticesAtingiveis = (int*) calloc(numVertices, sizeof(int));
+
+    // Adiciona arcos da origem para as moedas
+    for (int i = 0; i <= moedas[0].qtde; i++) {
+        int v = (i * moedas[0].valor) + 1;
+
+        if (v <= troco + 1) {
+            adicionaArco(g, 0, v, i * moedas[0].peso);
+            verticesAtingiveis[v] = 1;
+        }
+    }
+
+    // Adiciona arcos das moedas para as moedas
+    for (int k = 1; k < numMoedas; k++) {
+        for (int i = 1 + ((k - 1) * (troco + 1)); i < k * (troco + 1); i++) {
+            if (!verticesAtingiveis[i]) {
+                continue;
+            }
+            
+            for (int j = 0; j <= moedas[k].qtde; j++) {
+                int v = troco + i + (j * moedas[k].valor) + 1;
+
+                if (v <= (k + 1) * (troco + 1)) {
+                    adicionaArco(g, i, v, j * moedas[k].peso);
+                    verticesAtingiveis[v] = 1;
+                }
+            }
+        }
+    }
+
+    free(verticesAtingiveis);
+
+    // Adiciona arcos das moedas para o destino
+    for (int i = troco + 1; i < numVertices; i = i + troco + 1) {
+        adicionaArco(g, i, numVertices - 1, 0);
+    }
+
+    return g;
+}
+
+int main(int argc, char* argv[]) {
+    FILE* arquivo = fopen(argv[1], "r");
+
+    int n, Q;
+    fscanf(arquivo, "%d %d", &n, &Q);
+
+    Moeda* moedas = (Moeda*) malloc(n * sizeof(Moeda));
+    for (int i = 0; i < n; i++) {
+        int valor, peso, qtde;
+        fscanf(arquivo, "%d %d %d", &valor, &peso, &qtde);
+        moedas[i] = (Moeda) {valor, peso, qtde};
+    }
+
+    int numVertices = 2 + n * (Q + 1);
+    Grafo* g = criarGrafo(moedas, n, numVertices, Q);
+    fclose(arquivo);
+
+    int* dist = caminhoMinimo(g, 0, numVertices - 1);
+    int pesoMinimo = dist[numVertices - 1];
+
+    if (pesoMinimo != INT_MAX) {
+        printf("%d\n", pesoMinimo);
+    } else {
+        int maiorValor = 0;
+        int pesoMinimoMaiorValor = INT_MAX;
+
+        for (int i = 0; i <= Q; i++) {
+            for (int j = Q - i; j < numVertices - 3 - i; j = j + Q + 1) {
+                if (Q - i - 1 < maiorValor) {
+                    break;
+                }
+
+                if (dist[j] < pesoMinimoMaiorValor) {
+                    maiorValor = Q - i - 1;
+                    pesoMinimoMaiorValor = dist[j];
+                }
+            }
+        }
+
+        printf("%d %d\n", maiorValor, pesoMinimoMaiorValor);
+    }
+
     destroiGrafo(g);
+    free(moedas);
+    free(dist);
 
     return 0;
 }
